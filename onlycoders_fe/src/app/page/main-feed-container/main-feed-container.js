c7295035259911a08c5f6cc249de4f98a21cd067
Durgesh Nandan <durgnan45@gmail.com>
2023-10-04 23:50:03 +0100

    Sidebar and Nav

---
+++ onlycoders_fe/src/app/page/main-feed-container/main-feed-container.js
@@ -0,0 +1,19 @@
+'use client'
+
+import React from 'react';
+import { Jumbotron } from 'reactstrap'
+
+const MainFeedContainer = (props) => {
+    
+    return(
+        <div>
+            <Jumbotron>
+                
+            </Jumbotron>
+
+        </div>
+    )
+}
+
+export default MainFeedContainer;
+
